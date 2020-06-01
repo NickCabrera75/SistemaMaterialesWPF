147c095a7b90f524977119cdc718bc60053a97c8
Nick Cabrera <nickcabrera75@gmail.com>
2020-06-01 12:20:31 -0400

    Agregar archivos de proyecto.

---
+++ DAL/Methods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univalle.Fie.Isi.BaseDeDatos3.DAL
+{
+   public class Methods
+    {
+
+    }
+}
